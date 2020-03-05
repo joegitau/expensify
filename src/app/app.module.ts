7d22bd259f9434479f9668f8f770c84709c06f1d
joseph.gitau <joseph.gitau@canter.fi>
2020-03-05 16:36:47 +0200

    add budget-list component + logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AddItemComponent } from './add-item/add-item.component';
+import { BudgetListComponent } from './budget-list/budget-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AddItemComponent
+    AddItemComponent,
+    BudgetListComponent
   ],
   imports: [
     BrowserModule,
